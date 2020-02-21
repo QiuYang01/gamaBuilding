fc4fcde1e5299de34cb249b82d6477da659aab22
QiuYang01 <947808343@qq.com>
2020-02-21 22:45:40 +0800

    账户管理开始

---
+++ src/components/AccountManage/AccountManage.vue
@@ -0,0 +1,107 @@
+<template>
+    <div style="width:100%;max-height:91vh;background-color: aliceblue;height:91vh;">
+        <!-- //表格 -->
+    <div style="width: 100%;min-height:80vh;">
+        <el-table :data="tableData.slice((currentPage-1)*10,currentPage*10)"  style="width: 100%;">
+            <el-table-column label="Date" prop="date"> </el-table-column>
+            <el-table-column label="Name" prop="name"> </el-table-column>
+            <el-table-column align="right">
+            <template slot="header" slot-scope="scope">
+                <el-input  v-model="search" size="mini" placeholder="输入关键字搜索"/>
+            </template>
+            <template slot-scope="scope">
+                <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+                <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+            </template>
+            </el-table-column>
+        </el-table>
+    </div>
+
+        <!-- 分页 -->
+         <div class="block">
+            <el-pagination
+            @current-change="handleCurrentChange"
+            :current-page="currentPage"
+            layout="total, prev, pager, next, jumper"
+            :total="tableData.length">
+            </el-pagination>
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+    data() {
+      return {
+        currentPage: 1, //当前页
+         //数据总数
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }],
+        search: ''
+      };
+    },
+    methods: {
+      handleCurrentChange(val) {
+        //console.log(val);
+        console.log(`当前页: ${val}`);
+        this.currentPage = val;
+      },
+       handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      }
+    },
+    
+}
+</script>
+
+<style scope>
+body {
+    
+}
+</style>
